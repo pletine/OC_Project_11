42e5964f8a84ee0cb1705b38ca5bb7d7def64ca0
pletine <pierre.letinevez@gmail.com>
2023-08-08 09:54:18 +0200

    Creation of main files and structure of the site

---
+++ kasa/src/index.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import Home from './pages/Home'
+import Logement from './pages/Logements'
+import About from './pages/About'
+import Header from './components/Header'
+import Error from './components/Error'
+import Footer from './components/Footer'
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Router>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/logement/:questionNumber" element={<Logement />} />
+        <Route path="/about" element={<About />} />
+        <Route path="*" element={<Error />} />
+      </Routes>
+      <Footer />
+    </Router>
+  </React.StrictMode>,
+  document.getElementById('root')
+)
