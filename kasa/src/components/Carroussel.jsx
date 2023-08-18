0effe7e47de5c1d9fc7e4765e0a7b88238a903d1
pletine <pierre.letinevez@gmail.com>
2023-08-18 10:03:20 +0200

    Finish the functions for the carrousel

--- kasa/src/components/Carroussel.jsx
+++ kasa/src/components/Carroussel.jsx
@@ -1,35 +1,31 @@
+import React, { useState } from 'react';
+
 import '../styles/Carroussel.scss';
 import previous from '../assets/arrow_previous.svg';
 import next from '../assets/arrow_next.svg';
 
-let indexCurrentImage = 0;
-
-function prevImage() {
-  indexCurrentImage--;
-  console.log(indexCurrentImage);
-}
-
-function nextImage() {
-  indexCurrentImage++;
-  console.log(indexCurrentImage);
-}
-
 function Carroussel(props) {
-  indexCurrentImage = 0;
-
   const listImages = props.images;
-  const imagesDivs = listImages.map((image) =>
-    listImages.indexOf(image) === 0 ? (
-      <img src={image} alt="" key={image} className="carrou-show" />
-    ) : (
-      <img src={image} alt="" key={image} className="carrou-hidden" />
-    )
-  );
+  const [currentImageIdx, setCurrentImageIdx] = useState(0);
+
+  const prevImage = () => {
+    setCurrentImageIdx((prevIdx) =>
+      prevIdx === 0 ? listImages.length - 1 : prevIdx - 1
+    );
+  };
+
+  const nextImage = () => {
+    setCurrentImageIdx((prevIdx) =>
+      prevIdx === listImages.length - 1 ? 0 : prevIdx + 1
+    );
+  };
 
   return (
     <div className="carroussel">
       <img src={previous} alt="Précédent" onClick={prevImage} />
-      <div className="box_images">{imagesDivs}</div>
+      <div className="box_image">
+        <img src={listImages[currentImageIdx]} alt={`${currentImageIdx}`} />
+      </div>
       <img src={next} alt="Suivant" onClick={nextImage} />
     </div>
   );
